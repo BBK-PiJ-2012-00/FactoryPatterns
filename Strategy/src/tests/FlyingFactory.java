026a8d8260c904e4b48df7c9289fc08ef0f23ef4
Anna Taylor <anna.taylor@dcs-bbk-ac-uk>
2013-02-12 15:31:24 +0000

    Added model airplane functionality.

--- Strategy/src/tests/FlyingFactory.java
+++ Strategy/src/tests/FlyingFactory.java
@@ -1,6 +1,7 @@
 package tests;
 
 import vehicles.Flying;
+import vehicles.IDontFly;
 import vehicles.IFlyLikeFJ;
 
 public class FlyingFactory {
@@ -8,6 +9,9 @@ public class FlyingFactory {
 	public Flying createFlying(String string) {
 		if (string.equals("Fighter Jet")) {
 			return new IFlyLikeFJ();
+		}
+		if (string.equals("Model Airplane")) {
+			return new IDontFly();
 		}
 		return null;		
 	}
